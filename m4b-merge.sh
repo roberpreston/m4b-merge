6725b0b1c4d27205c59532a7efbb9abf2173ddeb
d2dyno <d2dyno@protonmail.com>
2019-03-29 20:47:05 -0500

    add warn for missing cookies

--- m4b-merge.sh
+++ m4b-merge.sh
@@ -117,6 +117,9 @@ function audibleparser() {
 		done
 	fi
 	if [[ ! -s $AUDMETAFILE ]] || [[ -s $AUDMETAFILE && $useoldmeta == "n" ]]; then
+		if [[ ! -s /tmp/aud-cookies.txt ]]; then
+			echo "WARN: Cookie file missing. This may lead to certain elements not working (like series and book numbering)"
+		fi
 		echo "Fetching metadata from Audible..."
 		curl -L -H "User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)" --cookie /tmp/aud-cookies.txt https://www.audible.com/pd/$ASIN -s -o "$AUDMETAFILE"
 	fi
