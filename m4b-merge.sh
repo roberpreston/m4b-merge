e2e7f2dfb5b5d360b7392a496861923828073089
d2dyno <d2dyno@protonmail.com>
2019-06-05 20:47:02 -0500

    set cookie message to error instead of warn

--- m4b-merge.sh
+++ m4b-merge.sh
@@ -135,7 +135,7 @@ function audibleparser() {
 	fi
 	if [[ ! -s $AUDMETAFILE ]] || [[ -s $AUDMETAFILE && $useoldmeta == "n" ]]; then
 		if [[ ! -s $AUDCOOKIES ]]; then
-			warn "Cookie file missing. This may lead to certain elements not working (like series and book numbering)"
+			error "Cookie file missing. This may lead to certain elements not working (like series and book numbering)"
 		fi
 		color_action "Fetching metadata from Audible..."
 		curl -L -H "User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)" --cookie $AUDCOOKIES https://www.audible.com/pd/$ASIN -s -o "$AUDMETAFILE"
